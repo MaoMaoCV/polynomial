d1acf2c67f14ecb85eba3f700c960efc64932987
MaoMaoMSAI <81927994+MaoMaoCV@users.noreply.github.com>
2024-01-27 15:52:49 -0500

    cs 506 exercise part b

--- polynomial.py
+++ polynomial.py
@@ -24,16 +24,57 @@ class Mul:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
-    
+
     def __repr__(self):
-        if isinstance(self.p1, Add):
-            if isinstance(self.p2, Add):
-                 return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
-            return "( " + repr(self.p1) + " ) * " + repr(self.p2)
-        if isinstance(self.p2, Add):
-            return repr(self.p1) + " * ( " + repr(self.p2) + " )"
-        return repr(self.p1) + " * " + repr(self.p2)
+        # Adding parentheses around p1 and p2 if they are instances of Add, Sub, Mul or Div
+        if isinstance(self.p1, (Add, Sub, Mul, Div)):
+            p1_repr = "( " + repr(self.p1) + " )"
+        else:
+            p1_repr = repr(self.p1)
 
+        if isinstance(self.p2, (Add, Sub, Mul, Div)):
+            p2_repr = "( " + repr(self.p2) + " )"
+        else:
+            p2_repr = repr(self.p2)
 
-poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
-print(poly)
+        return p1_repr + " * " + p2_repr
+
+# The newly added Sub and Div classes
+class Sub:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def __repr__(self):
+        # Adding parentheses if p2 is an instance of Add, Sub, Mul or Div
+        if isinstance(self.p2, (Add, Sub, Mul, Div)):
+            return repr(self.p1) + " - (" + repr(self.p2) + ")"
+        return repr(self.p1) + " - " + repr(self.p2)
+
+class Div:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def __repr__(self):
+        # Adding parentheses around p1 and p2 if they are instances of Add, Sub, or Mul
+        if isinstance(self.p1, (Add, Sub, Mul)):
+            p1_repr = "( " + repr(self.p1) + " )"
+        else:
+            p1_repr = repr(self.p1)
+
+        if isinstance(self.p2, (Add, Sub, Mul)):
+            p2_repr = "( " + repr(self.p2) + " )"
+        else:
+            p2_repr = repr(self.p2)
+
+        return p1_repr + " / " + p2_repr
+
+# Testing the new classes with various polynomial expressions
+poly1 = Sub(Div(X(), Int(2)), Add(X(), Int(3)))
+poly2 = Div(Add(X(), Mul(Int(2), X())), Int(5))
+poly3 = Mul(Sub(X(), Int(4)), Div(Int(3), Add(X(), Int(1))))
+
+print(poly1)
+print(poly2)
+print(poly3)
